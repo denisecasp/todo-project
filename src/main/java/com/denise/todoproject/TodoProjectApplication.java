14ddf81c85614fa9af666ca50199c0e415f42fc8
denisecasp <den.casperson@gmail.com>
2023-04-18 22:10:30 +0200

    My first commit

--- src/main/java/com/denise/todoproject/TodoProjectApplication.java
+++ src/main/java/com/denise/todoproject/TodoProjectApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoProjectApplication {
 
+    // Test test for first commit
+
     public static void main(String[] args) {
         SpringApplication.run(TodoProjectApplication.class, args);
     }
